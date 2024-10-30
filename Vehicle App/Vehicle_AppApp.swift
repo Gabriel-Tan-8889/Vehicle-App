4709d92975c4e943060a3f43d022512d5811407c
Gabriel Tan <gabriel.tanhk@gmail.com>
2024-10-30 19:14:29 +0800

    Initial Commit

---
+++ Vehicle App/Vehicle_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vehicle_AppApp.swift
+//  Vehicle App
+//
+//  Created by Jonaven Tan on 30/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Vehicle_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
